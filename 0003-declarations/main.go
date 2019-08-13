2c8727b85b233f6da4006667413340bd716462a1
mkannekanti <mohan.kannekanti@gmail.com>
2019-08-12 20:40:53 -0700

    GOpher is now taking decisions

--- 0003-declarations/main.go
+++ 0003-declarations/main.go
@@ -21,7 +21,11 @@ var safe = 0
 var z int
 
 // same as 'var z0 string = "100"'
+// Zero value of String is an empty string
 var z0 = "100"
+
+// Boolean declaration with Zero value of "false"
+var isSafe bool
 
 func main() {
 
@@ -38,17 +42,28 @@ func main() {
 	z = y + g1 + g2 + g3 + 10
 	fmt.Println("You now have", z, "GOold coins!! Enjoy my 10 GOld coins bonus")
 
-	safebox()
+	// Zero value is false
+	if !isSafe {
+		fmt.Println("Caution!! Your GOld coins are not safe in this world. Try to put them in safe.")
+		isSafe = safebox()
+
+		if isSafe {
+			fmt.Println("Congratulations on keeping your GOld coins in the Safebox.")
+		}
+	}
 }
 
-func safebox() {
+// function "safebox()" takes no arguments and returns boolean
+func safebox() bool {
 	// Accesing Package Level var variables in other functions
 	safe = z
 
 	// C-like printing variables.
 	fmt.Printf("These %#v GOld coins are of brand '%T'. Let's add them to your Safebox \n", safe, safe)
-	fmt.Printf("Amazon just delivered %#v GOld coins of branch '%T'\n", z0, z0)
+	fmt.Printf("Yay!! Amazon just delivered %#v GOld coins of branch '%T'\n", z0, z0)
 
 	fmt.Println("Sorry! Our safebox currently takes only 'int' brand. Throw them out of GO World.")
 	fmt.Println("Total worth in your Safebox is now:", safe)
+
+	return true
 }
